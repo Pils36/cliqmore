3854274fa0782a921b996f014950b074ef5a6ea5
Adebambo Adenuga <adenugaadebambo41@gmail.com>
2021-01-19 17:06:22 +0100

    cors middleware

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,6 @@ use Illuminate\Http\Request;
 //     return $request->user();
 // });
 
-header('Access-Control-Allow-Origin: *');
-header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
-
 
 Route::group(['prefix' => '/'], function(){
 
